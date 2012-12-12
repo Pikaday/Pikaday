84adccb2ee99592618712c342697e9e318cd0b87
Stas SUȘCOV <stas@net.utcluj.ro>
2012-12-13 01:18:34 +0200

    Refactor `toString` method to include time if was set.

--- pikaday.js
+++ pikaday.js
@@ -603,7 +603,16 @@
          */
         toString: function(format)
         {
-            return !isDate(this._d) ? '' : hasMoment ? window.moment(this._d).format(format || this._o.format) : this._d.toDateString();
+            var value = '';
+
+            if (isDate(this._d)) {
+                if (hasMoment) {
+                    value = window.moment(this._d).format(format || this._o.format);
+                } else {
+                    value = this._o.showTimePicker ? this._d.toString() : this._d.toDateString();
+                }
+            }
+            return value;
         },
 
         /**
