2305b36cb30c578d331a3123ed265c7964c3916a
John Arnfield <john.arnfield@bskyb.com>
2014-08-01 09:41:21 +0100

    Add disableDayFn option. A callback function that can be used to disable specific days

--- pikaday.js
+++ pikaday.js
@@ -588,6 +588,8 @@
 
             opts.disableWeekends = !!opts.disableWeekends;
 
+            opts.disableDayFn = (typeof opts.disableDayFn) == "function" ? opts.disableDayFn : null;
+
             var nom = parseInt(opts.numberOfMonths, 10) || 1;
             opts.numberOfMonths = nom > 4 ? 4 : nom;
 
@@ -921,10 +923,20 @@
             for (var i = 0, r = 0; i < cells; i++)
             {
                 var day = new Date(year, month, 1 + (i - before)),
-                    isDisabled = (opts.minDate && day < opts.minDate) || (opts.maxDate && day > opts.maxDate) || (opts.disableWeekends && isWeekend(day)),
                     isSelected = isDate(this._d) ? compareDates(day, this._d) : false,
                     isToday = compareDates(day, now),
                     isEmpty = i < before || i >= (days + before);
+
+                var isDisabled = false;
+                if (opts.minDate && day < opts.minDate) {
+                    isDisabled = true;
+                } else if (opts.maxDate && day > opts.maxDate) {
+                    isDisabled = true;
+                } else if (opts.disableWeekends && isWeekend(day)) {
+                    isDisabled = true;
+                } else if (opts.disableDayFn && opts.disableDayFn(day)) {
+                    isDisabled = true;
+                }
 
                 row.push(renderDay(1 + (i - before), month, year, isSelected, isToday, isDisabled, isEmpty));
 
