a9b685a9e465336a6abea3bb1ca25eb02a273ec8
Mohamed Sadek <mohamed@getyourguide.com>
2015-02-10 16:39:10 +0100

    Added experiment classes - removed Disabled attr

--- pikaday.js
+++ pikaday.js
@@ -266,7 +266,7 @@
         return abbr ? opts.i18n.weekdaysShort[day] : opts.i18n.weekdays[day];
     },
 
-    renderDay = function(d, m, y, isSelected, isToday, isDisabled, isEmpty, isAvailable)
+    renderDay = function(d, m, y, isSelected, isToday, isDisabled, isEmpty, isAvailable, isShowAvailability)
     {
         var attr = '';
         if (isEmpty) {
@@ -284,7 +284,9 @@
         }
         if (!isAvailable) {
             arr.push('not-available');
-            attr = 'disabled ';
+            if(isShowAvailability === false) {
+                attr = 'disabled ';
+            }
         }
         return '<td data-day="' + d + '" class="' + arr.join(' ') + '">' +
                  '<button class="pika-button pika-day" type="button" ' + attr +
@@ -524,6 +526,9 @@
 
         addEvent(self.el, 'mousedown', self._onMouseDown, true);
         addEvent(self.el, 'change', self._onChange);
+        if(opts.showAvailability === true) {
+            self.el.className = 'pika-single future-available' + (opts.isRTL ? ' is-rtl' : '');
+        }
 
         if (opts.field) {
             if (opts.container) {
@@ -942,9 +947,10 @@
                     isSelected = isDate(this._d) ? compareDates(day, this._d) : false,
                     isToday = compareDates(day, now),
                     isEmpty = i < before || i >= (days + before),
-                    isAvailable = (typeof opts.isAvailable === 'function') ? opts.isAvailable(day) : true;
+                    isAvailable = (typeof opts.isAvailable === 'function') ? opts.isAvailable(day) : true,
+                    isShowAvailability = opts.showAvailability;
 
-                row.push(renderDay(1 + (i - before), month, year, isSelected, isToday, isDisabled, isEmpty, isAvailable));
+                row.push(renderDay(1 + (i - before), month, year, isSelected, isToday, isDisabled, isEmpty, isAvailable, isShowAvailability));
 
                 if (++r === 7) {
                     if (opts.showWeekNumber) {
