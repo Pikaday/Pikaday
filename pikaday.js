d3f2f01c6fe9c0f5261c8bb46ab679afaf1e1434
Stephen Watkins <stjowa@gmail.com>
2016-10-28 03:27:26 -0400

    Only set attribute if field is bound (#567)

--- pikaday.js
+++ pikaday.js
@@ -981,8 +981,11 @@
             if (typeof this._o.onDraw === 'function') {
                 this._o.onDraw(this);
             }
-          // let the screen reader user know to use arrow keys
-          this._o.field.setAttribute('aria-label', 'Use the arrow keys to pick a date');
+            
+            if (opts.bound) {
+                // let the screen reader user know to use arrow keys
+                opts.field.setAttribute('aria-label', 'Use the arrow keys to pick a date');
+            }
         },
 
         adjustPosition: function()
