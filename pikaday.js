efd3ef401224ecc38cb23c91c66f65fa7b12b6b1
Faisal Rahman <id.faisalrahman@gmail.com>
2018-06-26 16:35:03 +0700

    comment out optional moment import

--- pikaday.js
+++ pikaday.js
@@ -6,26 +6,26 @@
 
 (function (root, factory)
 {
-    'use strict';
+    // 'use strict';
 
-    var moment;
-    if (typeof exports === 'object') {
-        // CommonJS module
-        // Load moment.js as an optional dependency
-        try { moment = require('moment'); } catch (e) {}
-        module.exports = factory(moment);
-    } else if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
-        define(function (req)
-        {
-            // Load moment.js as an optional dependency
-            var id = 'moment';
-            try { moment = req(id); } catch (e) {}
-            return factory(moment);
-        });
-    } else {
-        root.Pikaday = factory(root.moment);
-    }
+    // var moment;
+    // if (typeof exports === 'object') {
+    //     // CommonJS module
+    //     // Load moment.js as an optional dependency
+    //     try { moment = require('moment'); } catch (e) {}
+    //     module.exports = factory(moment);
+    // } else if (typeof define === 'function' && define.amd) {
+    //     // AMD. Register as an anonymous module.
+    //     define(function (req)
+    //     {
+    //         // Load moment.js as an optional dependency
+    //         var id = 'moment';
+    //         try { moment = req(id); } catch (e) {}
+    //         return factory(moment);
+    //     });
+    // } else {
+    //     root.Pikaday = factory(root.moment);
+    // }
 }(this, function (moment)
 {
     'use strict';
